732b8f84e3ac0052916adeed26134ad1333d7df6
max delzer <emma.delzer@gmail.com>
2019-07-17 20:38:00 -0700

    App listener added to server

--- server.js
+++ server.js
@@ -8,3 +8,10 @@ var PORT = process.env.PORT || 8080;
 
 // Create express app instance.
 var app = express();
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
