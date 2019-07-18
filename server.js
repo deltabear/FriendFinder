8a4903c805e2eb3903906234f557fde307581125
max delzer <emma.delzer@gmail.com>
2019-07-17 20:34:49 -0700

    Required npm packages and set up app port

---
+++ server.js
@@ -0,0 +1,10 @@
+// Your server.js file should require the basic npm packages we've used in class: express and path.
+var express = require("express");
+var path = require("path");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Create express app instance.
+var app = express();
