9418424ac8a927575b9c0f3c7214359fd704805d
max delzer <emma.delzer@gmail.com>
2019-07-19 20:50:15 -0700

    export and routes set up in htmlRoutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,3 +2,25 @@
 
 // A GET Route to /survey which should display the survey page.
 // A default, catch-all route that leads to home.html which displays the home page.
+
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // these pages are fetched when users go to page url
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  // the home default if the user doesn't go to any page
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
